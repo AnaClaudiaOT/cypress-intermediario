6892cb353d9c9272d64f4ff7c785ebd66d1b3f43
AnaClaudiaOT <ana.claudia.tavares@hotmail.com>
2024-01-09 15:19:42 -0300

    cypress-intermediario

---
+++ cypress/e2e/gui/createIssue.cy.js
@@ -0,0 +1,27 @@
+import { faker } from '@faker-js/faker'
+
+describe('Create Issue', () => {
+  beforeEach(() => cy.api_deleteProjects())
+  const issue = {
+    title: `issue-${faker.datatype.uuid()}`,
+    description: faker.random.words(3),
+    project: {
+      name: `project-${faker.datatype.uuid()}`,
+      description: faker.random.words(5)
+    }
+  }
+
+  beforeEach(() => {
+    cy.login()
+    cy.api_createProject(issue.project)
+  })
+
+  it('successfully', () => {
+    cy.gui_createIssue(issue)
+
+    cy.get('.issue-details')
+      .should('contain', issue.title)
+      .and('contain', issue.description)
+      
+  })
+})
---
+++ cypress/e2e/gui/login.cy.js
@@ -0,0 +1,18 @@
+describe('Login', () => {
+  it('successfully', () => {
+    const user = Cypress.env('user_name')
+    const password = Cypress.env('user_password')
+    const options = { cacheSession: false }
+
+    cy.login(user, password, options)
+
+    cy.get('.qa-user-avatar').should('be.visible')
+  })
+})
+
+
+// describe('empty spec', () => {
+//   it('passes', () => {
+//     cy.visit('https://example.cypress.io')
+//   })
+// })
